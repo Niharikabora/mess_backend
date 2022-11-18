49e60a50b2f8bb8e5b4364a227dff8daa9520bee
Niharika Bora <niharikabora912@gmail.com>
2022-11-18 10:52:34 +0530

    v6

--- config/db.js
+++ config/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const config = require('config')
 
-const db = config.get('mongoURI');
+// const db = config.get('mongoURI');
+const db = "mongodb+srv://niharika:qwerty420@cluster0.brfruyl.mongodb.net/?retryWrites=true&w=majority"
 
 const connectDB = async() =>{
     try {
